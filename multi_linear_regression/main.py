6b9ba44b9c53631fc046349530fb87f87e84f881
omri wallach <omriwallach1992@gmail.com>
2020-12-28 13:09:50 +0100

    remove visualization

--- multi_linear_regression/main.py
+++ multi_linear_regression/main.py
@@ -26,14 +26,3 @@ regressor.fit(X_train,Y_train)
 
 #  Test
 y_pred = regressor.predict(X_test)
-
-# Visualsing
-import  matplotlib.pyplot as plt
-
-# plt.scatter(X_train,Y_train, color='red')
-# plt.scatter(X_test,y_pred, color="green")
-# plt.plot(X_train,regressor.predict(X_train),color="blue")
-# plt.title("salary vs experience")
-# plt.xlabel("years of experience")
-# plt.xlabel("salary")
-# plt.show()
