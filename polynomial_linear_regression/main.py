7289e857225c8a6e4cb18e41c0716d5a7eb09e93
omri wallach <omriwallach1992@gmail.com>
2020-12-28 13:38:30 +0100

    remove hot encoding

--- polynomial_linear_regression/main.py
+++ polynomial_linear_regression/main.py
@@ -2,21 +2,8 @@
 import pandas as pd
 
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:,:-1].values
+X = dataset.iloc[:,1:-1].values
 Y = dataset.iloc[:,-1].values
-
-# hot encoding
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-import numpy as np
-
-ct = ColumnTransformer(transformers=[("encoder",OneHotEncoder(),[0])],remainder="passthrough")
-X = np.array(ct.transform(X))
-# splitting the dataset
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=0, test_size=0.2)
-
 # import regression model
 from sklearn.linear_model import LinearRegression as lr
 
