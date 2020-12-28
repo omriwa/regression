b8c6726a19e240902894b400218eae729eb8eab1
omri wallach <omriwallach1992@gmail.com>
2020-12-28 14:05:47 +0100

    visualize polyline

--- polynomial_linear_regression/main.py
+++ polynomial_linear_regression/main.py
@@ -6,20 +6,26 @@ X = dataset.iloc[:,1:-1].values
 Y = dataset.iloc[:,-1].values
 # import regression model
 from sklearn.linear_model import LinearRegression as lr
+from sklearn.preprocessing import PolynomialFeatures
 
 regressor = lr()
-regressor.fit(X_train,Y_train)
+regressor.fit(X,Y)
+
+poly_reg = PolynomialFeatures(degree=5)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = lr()
+lin_reg_2.fit(X_poly,Y)
 
 #  Test
-y_pred = regressor.predict(X_test)
+y_pred = regressor.predict(X)
 
 # Visualsing
 import  matplotlib.pyplot as plt
 
-plt.scatter(X_train,Y_train, color='red')
-plt.scatter(X_test,y_pred, color="green")
-plt.plot(X_train,regressor.predict(X_train),color="blue")
-plt.title("salary vs experience")
-plt.xlabel("years of experience")
+plt.scatter(X,Y, color="green")
+plt.plot(X,regressor.predict(X),color="blue")
+plt.plot(X,lin_reg_2.predict(X_poly),color="red")
+plt.title("salary vs level")
+plt.xlabel("level")
 plt.xlabel("salary")
 plt.show()
