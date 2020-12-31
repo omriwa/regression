8f04ba081c2cce316755546dc186b54c8b6257e9
omri wallach <omriwallach1992@gmail.com>
2020-12-31 12:57:30 +0100

    predict svr

--- svr/main.py
+++ svr/main.py
@@ -1,31 +1,35 @@
 # import dataset
 import pandas as pd
+import numpy as np
 
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:,:-1].values
+X = dataset.iloc[:,1].values
 Y = dataset.iloc[:,-1].values
+X = X.reshape(len(X),1)
+Y = Y.reshape(len(Y),1)
 
-# splitting the dataset
-from sklearn.model_selection import train_test_split
+# feature scaling
+from sklearn.preprocessing import StandardScaler
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=0, test_size=0.2)
+x_scaler = StandardScaler()
+y_scaler = StandardScaler()
+X = x_scaler.fit_transform(X)
+Y = y_scaler.fit_transform(Y)
 
 # import regression model
-from sklearn.linear_model import LinearRegression as lr
+from sklearn.svm import SVR
 
-regressor = lr()
-regressor.fit(X_train,Y_train)
+regressor = SVR(kernel="rbf",)
+regressor.fit(X,Y)
 
 #  Test
-y_pred = regressor.predict(X_test)
+y_pred = y_scaler.inverse_transform(regressor.predict(x_scaler.transform([[6.5]]))) 
 
 # Visualsing
 import  matplotlib.pyplot as plt
 
-plt.scatter(X_train,Y_train, color='red')
-plt.scatter(X_test,y_pred, color="green")
-plt.plot(X_train,regressor.predict(X_train),color="blue")
-plt.title("salary vs experience")
-plt.xlabel("years of experience")
-plt.xlabel("salary")
-plt.show()
+
+# plt.title("salary vs level")
+# plt.xlabel("level")
+# plt.xlabel("salary")
+# plt.show()
