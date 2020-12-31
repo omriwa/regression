d4dbcb11d6960b74c2bd3975e256ee1f71c4494a
omri wallach <omriwallach1992@gmail.com>
2020-12-31 13:33:27 +0100

    svr poly visualize

--- svr/main.py
+++ svr/main.py
@@ -34,3 +34,15 @@ plt.title("salary vs level")
 plt.xlabel("level")
 plt.xlabel("salary")
 plt.show()
+
+from  sklearn.preprocessing import PolynomialFeatures
+
+X_grid = np.arange(min(x_scaler.inverse_transform(X)),max(x_scaler.inverse_transform(X)),0.1)
+X_grid = X_grid.reshape(len(X_grid),1)
+poly_scaler = PolynomialFeatures(degree=5)
+plt.scatter(x_scaler.inverse_transform(X),y_scaler.inverse_transform(Y), color="green")
+plt.plot(X_grid,y_scaler.inverse_transform(regressor.predict(x_scaler.transform(X_grid))),color="blue")
+plt.title("salary vs level 2")
+plt.xlabel("level")
+plt.xlabel("salary")
+plt.show()    
